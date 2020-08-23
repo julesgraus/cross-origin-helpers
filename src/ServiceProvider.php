c84a4a27de3da6ac9bb778547675cb7e52d57609
Jules Graus <jules.graus@gmail.com>
2020-08-23 22:35:44 +0200

    Initial version

---
+++ src/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Class ServiceProvider
+ *
+ *
+ *
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
